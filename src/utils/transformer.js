a914f1334c5eac5bfc98c259a2779e5bc829270d
priyanka-pawar-05 <62424247+priyanka-pawar-05@users.noreply.github.com>
2020-03-22 19:00:34 +0530

    Add files via upload

--- src/utils/transformer.js
+++ src/utils/transformer.js
@@ -4,6 +4,6 @@ export const initialData = {
     filters: moviesdata.components.filter(item => item.type === "order-select").pop().items,
     movies: moviesdata.components.filter(item => item.type === "movie-list").pop().items,
 }
-export const sortByType = (moviesdata, type) => {             //sorting data based on filter type
+export const sortByType = (moviesdata, type) => {                   //sorting data based on filter type
     return moviesdata.sort((a, b) => a[type] - b[type]);
 }
