025fda97379b28824e8ee00aa965078684abbda6
priyanka-pawar-05 <62424247+priyanka-pawar-05@users.noreply.github.com>
2020-03-22 18:59:22 +0530

    Add files via upload

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import metadata from './metadata'
 import filter from './filter'
 import poster from './poster'
+
 export default combineReducers({
  metadata,
  filter,
